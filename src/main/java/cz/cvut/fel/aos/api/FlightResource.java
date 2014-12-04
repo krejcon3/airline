fd0467ce4eba539fae5cd4913624d220f8205a30
Ondřej Krejčíř <krejcon3@gmail.com>
2014-12-04 13:06:32 +0100

    FlightResource: Update&Create bere vzdálenost z DistanceAPi

--- src/main/java/cz/cvut/fel/aos/api/FlightResource.java
+++ src/main/java/cz/cvut/fel/aos/api/FlightResource.java
@@ -1,10 +1,9 @@
 package cz.cvut.fel.aos.api;
 
+import cz.cvut.fel.aos.api.data.Destination;
 import cz.cvut.fel.aos.api.data.Flight;
 import cz.cvut.fel.aos.persistence.PersistenceException;
-import cz.cvut.fel.aos.service.FlightService;
-import cz.cvut.fel.aos.service.ServiceException;
-import cz.cvut.fel.aos.service.UserService;
+import cz.cvut.fel.aos.service.*;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -19,10 +18,14 @@ public class FlightResource {
 
 	protected FlightService service;
 	protected UserService userService;
+	protected DestinationService destinationService;
+	protected DistanceService distanceService;
 
 	public FlightResource() {
 		this.service = new FlightService();
 		this.userService = new UserService();
+		this.destinationService = new DestinationService();
+		this.distanceService = new DistanceService();
 	}
 
 	@GET
@@ -75,7 +78,7 @@ public class FlightResource {
 	public Response saveFlight(@HeaderParam("Authorization") String authorization, Flight data) {
 		try {
 			this.userService.authenticate(authorization);
-			this.service.create(data);
+			this.service.create(this.decorateDistance(data));
 			return Response.status(Response.Status.OK).entity("Flight " + data.getId() + " created.").type(MediaType.APPLICATION_JSON).build();
 		} catch(PersistenceException e) {
 			return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).type(MediaType.APPLICATION_JSON).build();
@@ -92,7 +95,7 @@ public class FlightResource {
 	public Response updateFlight(@HeaderParam("Authorization") String authorization, @PathParam("id") Long id, Flight data) {
 		try {
 			this.userService.authenticate(authorization);
-			this.service.update(id, data);
+			this.service.update(id, this.decorateDistance(data));
 			return Response.status(Response.Status.OK).entity("Flight " + data.getId() + " updated.").type(MediaType.APPLICATION_JSON).build();
 		} catch(PersistenceException e) {
 			return Response.status(Response.Status.BAD_REQUEST).entity(e.getMessage()).type(MediaType.APPLICATION_JSON).build();
@@ -116,5 +119,14 @@ public class FlightResource {
 		} catch (UnauthorizedException e) {
 			return Response.status(Response.Status.UNAUTHORIZED).entity(e.getMessage()).type(MediaType.APPLICATION_JSON).build();
 		}
+	}
+
+	private Flight decorateDistance(Flight flight) {
+		Destination from = this.destinationService.find((long)flight.getFrom());
+		Destination to = this.destinationService.find((long)flight.getTo());
+		Double distance = this.distanceService.getDistance(new double[]{from.getLatitude(), from.getLatitude()}, new double[]{to.getLatitude(), to.getLatitude()});
+		flight.setDistance(distance);
+		flight.setPrice(distance * 10);
+		return flight;
 	}
 }
